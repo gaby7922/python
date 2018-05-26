75ce97e221a64295d152c58b4e3ec8a7fd0bd2b1
Nicolas Ortiz <nortiz2010@gmail.com>
2018-05-26 12:55:00 -0400

    primer_commit

---
+++ ejemplo.py
@@ -0,0 +1,10 @@
+#! C:\Python27
+# -*- coding: iso-8859-15 -*-
+import os, sys
+num_1 = int(input("Introduce el primer numero a sumar :"))
+num_2 = int(input("Introduce el segundo numero a sumar :"))
+num_3 = int(input("Introduce el tercer numero a sumar :"))
+num_4 = int(input("Introduce el cuarto numero a sumar :"))
+suma = (num_1 + num_2 + num_3 + num_4)
+palabra = "total sumatoria="
+print(palabra, suma )
---
+++ suma.py
@@ -0,0 +1,6 @@
+#! C:\Python27
+# -*- coding: iso-8859-15 -*-
+import os, sys
+a = int(input("unidades :"))
+b = int(input("precio :"))
+print("A pagar=", a*b)
